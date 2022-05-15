277e9a17eee046dd8edc6ae3252c994e78bcf740
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-05-15 16:15:30 +0800

    finish get ans

--- src/api/exam.js
+++ src/api/exam.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 const base_url="http://124.222.112.118:8085/";
 
 export function getExam(params=null){
-    return axios.get(base_url+'test/list/',params)
+    return axios.get(base_url+'test/list/',params);
+}
+
+export function postResults(params=null){
+    return axios.post(base_url+'test/',params);
 }
